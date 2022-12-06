1b37fba5a7e6ea1827e23e7b6dea08331e0dfba7
Milad <milad.4274@gmail.com>
2022-12-06 19:16:00 +0330

    debug hyoer tune argparser

--- hyper_tune.py
+++ hyper_tune.py
@@ -18,8 +18,8 @@ parser = argparse.ArgumentParser(
                     epilog = 'you can change only number of cpus and gpus using command line.')
 
 
-parser.add_argument('cpu', type=int, help="number of cpus to use for processing", default=2, required=False)           # positional argument
-parser.add_argument('gpu', type=int, help="number of gpus to use for processing", default=1, required=False)           # positional argument
+parser.add_argument('--cpu', type=int, help="number of cpus to use for processing", default=2, )           # positional argument
+parser.add_argument('--gpu', type=int, help="number of gpus to use for processing", default=1, required=False)           # positional argument
 
 
 args = parser.parse_args()
